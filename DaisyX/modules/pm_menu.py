0f1bf1aef9321a6bbc297c86d1625275cfe5f0a6
Spdpush <86373515+Spdpush@users.noreply.github.com>
2021-10-07 11:21:56 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -81,13 +81,13 @@ async def get_start_func(message, strings, edit=False):
     buttons.add(
         InlineKeyboardButton(strings["btn_lang"], callback_data="lang_btn"),
         InlineKeyboardButton(
-            strings["btn_source"], url="https://github.com/TeamDaisyX/"
+            strings["btn_source"], url="https://t.me/hexamyworld"
         ),
     )
     buttons.add(
-        InlineKeyboardButton(strings["btn_channel"], url="https://t.me/DaisyXUpdates"),
+        InlineKeyboardButton(strings["btn_channel"], url="https://t.me/hexamyworld"),
         InlineKeyboardButton(
-            strings["btn_group"], url="https://t.me/DaisySupport_Official"
+            strings["btn_group"], url="https://t.me/hexamyworld"
         ),
     )
     buttons.add(
